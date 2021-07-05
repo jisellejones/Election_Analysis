ef50ca6e3621623bfd041750161847b84f65fcd9
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-07-04 22:21:37 -0700

    Update files and add 2 files.

--- declare_votes.py
+++ declare_votes.py
@@ -2,6 +2,12 @@
 my_votes = int(input("How many votes did you get in the election? "))
 #  Total votes in the election
 total_votes = int(input("What is the total votes in the election? "))
-# Calculate the percentage of votes you received.
-percentage_votes = (my_votes / total_votes) * 100
-print("I received " + str(percentage_votes)+"% of the total votes.")
+message_to_candidate = (
+    f"You received {my_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {my_votes / total_votes * 100: .2f}% of the total vote."
+)
+print(message_to_candidate)
+# Calculate the percentage of votes you received - older code
+#percentage_votes = (my_votes / total_votes) * 100
+#print("I received " + str(percentage_votes)+"% of the total votes.")
