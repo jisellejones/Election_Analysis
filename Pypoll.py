fb758f755abb8bd7f5ef98af4b492121fea7bc15
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-07-07 17:17:34 -0700

    added winning candidate summary to Pypoll.py to print to election_analysis.txt

--- Pypoll.py
+++ Pypoll.py
@@ -68,6 +68,11 @@ with open(file_to_save, "w") as txt_file:
 
         #Print the candidate name and percentage of votes.
         #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
 
         # Determine winning vote count and candidate
         # Determine if the votes are greater than the winning count.
@@ -87,7 +92,12 @@ with open(file_to_save, "w") as txt_file:
         f"Winning Percentage: {winning_percentage:.2f}%\n"
         f"-------------------------\n")
 
-# print(winning_candidate_summary)
+    print(winning_candidate_summary)
+
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
+
+
 
 
 
